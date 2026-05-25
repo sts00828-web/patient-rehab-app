5f1c5793080e5dd2ee0733561b38e8d409a4af3c
komaitoreha <komaitoreha@gmail.com>
2026-05-25 13:15:31 +0900

    URL hash #d= に統一＋正規表現で生抽出（+保持）
    クエリパラメータ使用時に "+" がスペース化する問題が原因だった。
    - buildShareUrl: #d=<data> 形式で生成（ハッシュは + の扱いが安全）
    - extractImportData: 正規表現で生抽出。+ がスペース化されないよう
      URLSearchParams を使わない
    - ?d=, ?import=, #import= もすべて後方互換でサポート
    - manualImport: URLから生抽出、ハッシュ形式に変換して処理
    - SW: skipWaiting() で新SWを即時アクティブ化、v4に更新

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE = 'rehab-v3';
+const CACHE = 'rehab-v4';
 
 // 新しいSWをすぐにアクティブ化
 self.addEventListener('message', e => { if (e.data === 'skipWaiting') self.skipWaiting(); });
+self.addEventListener('install', e => self.skipWaiting());
 const FILES = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e =>
